301dfb2e2912641af80ed82023b4551ad273fb4d
DevAndreww <andrea.sette2@marconibari.edu.it>
2024-10-31 09:19:06 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+int main() 
+{
+    int i, k;
+    float media, livello, somma;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    i = 1;
+    k = 0;
+    somma = 0;
+    while (i <= 7) {
+        cout << "Quant'è il livello dell'acqua?" << endl;
+        cin >> livello;
+        if (livello > 10) {
+            cout << "L'acqua nel giorno " << i << " supera i 10 cm." << endl;
+            k = k + 1;
+        }
+        somma = somma + livello;
+        media = somma / 7;
+        i = i + 1;
+    }
+    if (media >= 10) {
+        cout << "Il livello medio dell'acqua è alto" << endl;
+    }
+    if (media <= 0) {
+        cout << "Il livello medio dell'acqua è normale" << endl;
+    }
+    cout << "L'acqua è maggiore di dieci, " << k << " volte, e la media è di " << media << "cm." << endl;
+
+}
+ 
